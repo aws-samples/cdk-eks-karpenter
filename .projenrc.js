c4179c0274cb2f76a51486438a2d667b88861437
Andreas Lindh <elindh@amazon.com>
2023-08-07 10:47:59 +0200

    Update deps

--- .projenrc.js
+++ .projenrc.js
@@ -8,7 +8,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   authorAddress: 'elindh@amazon.com',
   description: 'CDK construct library that allows you install Karpenter in an AWS EKS cluster',
   keywords: ['eks', 'karpenter'],
-  cdkVersion: '2.61.1',
+  cdkVersion: '2.89.0',
   devDeps: ['@aws-cdk/lambda-layer-kubectl-v24'],
   defaultReleaseBranch: 'main',
   name: PROJECT_NAME,
